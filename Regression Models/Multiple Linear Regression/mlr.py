36d4f969b1ed41f6aa33b3a5c6507da521e8be6b
Aditya Mandal <aditya.cse.nitap@gmail.com>
2018-09-25 15:04:08 +0530

    Implementation of Multiple Linear Regression to the dataset

--- Regression Models/Multiple Linear Regression/mlr.py
+++ Regression Models/Multiple Linear Regression/mlr.py
@@ -1,7 +1,8 @@
 """
 This is the Multiple Linear Regression implementation on the dataset for predicting F/R the result.
-Unfortunately the prediction with backward elimination method is not upto the mark. Infact the 
-dataset is 
+Unfortunately the prediction with backward elimination method is not doing actually what we want.
+Infact the dataset is not suitable for prediction via multiple linear regression model to predict
+better result of S/R with the following independent variables in the dataset. 
 """
 
 # Multiple Linear Regression
@@ -23,30 +24,30 @@ dataset['MonthOfYear'] = dataset['Date'].dt.month_name()
 dataset['Year'] = dataset['Date'].dt.year
 
 # taking independent variables and dependent variables
-x = dataset.iloc[:, [4,5,6]].values
-y = dataset.iloc[:, [1]].values
+x = dataset.iloc[:, [1,4,5,6]].values
+y = dataset.iloc[:, [2]].values
 
 # Encoding categorical data such as DayOfMonth, MonthOfYear, Year
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_x = LabelEncoder()
-x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
 x[:, 1] = labelencoder_x.fit_transform(x[:, 1])
 x[:, 2] = labelencoder_x.fit_transform(x[:, 2])
+x[:, 3] = labelencoder_x.fit_transform(x[:, 3])
 
 # one hot encoding the DayOfMonth
-onehotencoder = OneHotEncoder(categorical_features = [0])
+onehotencoder = OneHotEncoder(categorical_features = [1])
 x = onehotencoder.fit_transform(x).toarray()
 
 # one hot encoding the MonthOfYear
-onehotencoder = OneHotEncoder(categorical_features = [6])
+onehotencoder = OneHotEncoder(categorical_features = [7])
 x = onehotencoder.fit_transform(x).toarray()
 
 # one hot encoding the Year
-onehotencoder = OneHotEncoder(categorical_features = [18])
+onehotencoder = OneHotEncoder(categorical_features = [19])
 x = onehotencoder.fit_transform(x).toarray()
 
 # Avoiding Dummy Variable Trap
-x = x[:, [1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,19,20,21,22]]
+x = x[:, [1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,19,20,21,22,23]]
 
 # Splitting the dataset into training set and test set
 from sklearn.model_selection import train_test_split
@@ -60,90 +61,119 @@ regressor.fit(x_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(x_test)
 
+#visualising the test set without backward elimination method
+plt.scatter(x_test[:,[20]], y_test, color='red')
+plt.plot(x_test[:,[20]], regressor.predict(x_test), color='green')
+plt.title('F/R vs S/R (Without Backward Elimination Method)')
+plt.xlable('F/R')
+plt.ylabel('S/R')
+plt.show()
+
 # Building the optimal model using Backward Elimination
-"""import statsmodels.formula.api as sm
+import statsmodels.formula.api as sm
 x = np.append(arr = np.ones((1138, 1)).astype(int), values = x, axis = 1)
 x_opt = x[:, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]]
+# removin' index 14 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 3, 4, 5, 6, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]]
+# removin' index 11 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 3, 4, 5, 6, 8, 9, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21]]
+# removin' index 6 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 3, 4, 5, 6, 9, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21]]
+# removin' index 3 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 1, 2, 4, 5, 7, 8, 9, 10, 12, 13, 15, 16, 17, 18, 19, 20, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 3, 4, 5, 6, 9, 12, 13, 15, 16, 17, 19, 20, 21]]
+# removin' index 12 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 1, 2, 4, 5, 7, 8, 9, 10, 13, 15, 16, 17, 18, 19, 20, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 3, 4, 5, 6, 9, 12, 13, 15, 16, 17, 19, 20, 21]]
+# removin' index 15 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 1, 2, 4, 5, 7, 8, 9, 10, 13, 16, 17, 18, 19, 20, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 3, 4, 6, 9, 12, 13, 15, 16, 17, 19, 20, 21]]
+# removin' index 13 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 1, 2, 4, 5, 7, 8, 9, 10, 16, 17, 18, 19, 20, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4, 6, 9, 12, 13, 15, 16, 17, 19, 20, 21]]
+# removin' index 9 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 1, 2, 4, 5, 7, 8, 10, 16, 17, 18, 19, 20, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4, 6, 9, 12, 13, 15, 16, 17, 20, 21]]
+# removin' index 1 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 2, 4, 5, 7, 8, 10, 16, 17, 18, 19, 20, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4, 6, 9, 12, 13, 15, 16, 17, 20, 21]]
+# removin' index 20 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 2, 4, 5, 7, 8, 10, 16, 17, 18, 19, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4, 6, 9, 12, 13, 15, 16, 17, 21]]
+# removin' index 16 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 2, 4, 5, 7, 8, 10, 17, 18, 19, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4, 9, 12, 13, 15, 16, 17, 21]]
+# removin' index 17 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 2, 4, 5, 7, 8, 10, 18, 19, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4, 9, 12, 13, 15, 16, 21]]
+# removin' index 19 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 2, 4, 5, 7, 8, 10, 18, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4, 9, 12, 13, 16, 21]]
+# removin' index 10 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 2, 4, 5, 7, 8, 18, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4, 9, 12, 13, 16]]
+# removin' index 8 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 2, 4, 5, 7, 18, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4, 9, 12, 13]]
+# removin' index 2 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 4, 5, 7, 18, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4, 9, 12]]
+# removin' index 18 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 4, 5, 7, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4, 9]]
+# removin' index 5 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 4, 7, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 4]]
+# removin' index 7 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+x_opt = x[:, [0, 4, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 4]]
+# removin' index 4 predictor from the original x matrix because of highest p value for teamin' up x_opt with optimal predictors
+# Here we have got our optimal team of predictor by using backward elimination method
+x_opt = x[:, [0, 21]]
 regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regressor_OLS.summary()
 
@@ -157,4 +187,12 @@ regressor_opt = LinearRegression()
 regressor_opt.fit(x_opt_train, y_train)
 
 # Predicting the optimal test set results that will have the strong impact on predicting profit
-y_pred_opt = regressor_opt.predict(x_opt_test)"""
+y_pred_opt = regressor_opt.predict(x_opt_test)
+
+#visualising the test set with backward elimination method
+plt.scatter(x_opt_test[:,[1]], y_test, color='red')
+plt.plot(x_opt_test[:,[1]], y_pred_opt, color='green')
+plt.title('F/R vs S/R (With Backward Elimination Method)')
+plt.xlabel('F/R')
+plt.ylabel('S/R')
+plt.show()
