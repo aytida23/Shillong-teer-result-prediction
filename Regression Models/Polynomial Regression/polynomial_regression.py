d6836702ff157d14c3c1c7bdb1ab5cfe52248725
Aditya Mandal <aditya.cse.nitap@gmail.com>
2018-11-19 13:02:42 +0530

    updated the code
    Rightnow the r-squared value is 0.84 after implementing this polynomial regression model on the dataset

--- Regression Models/Polynomial Regression/polynomial_regression.py
+++ Regression Models/Polynomial Regression/polynomial_regression.py
@@ -49,7 +49,7 @@ x = x[:, [1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,19,20,21,22,23]]
 
 # Splitting the dataset into training set and test set
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 101)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 5)
 
 # Fitting linear regression to the dataset
 from sklearn.linear_model import LinearRegression
